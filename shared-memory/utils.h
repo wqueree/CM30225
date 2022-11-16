5b5016d3c8182993048626ad8f23d88a2253311d
Will Queree <wqueree@gmail.com>
2022-11-16 16:08:43 +0000

    utils.h commented

--- shared-memory/utils.h
+++ shared-memory/utils.h
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define BILLION 1000000000L
+#define BILLION 1000000000L // Used in time calculations
 
-#define LOGGING false
-#define PRECISION 0.01
+#define LOGGING false // Switch for logging
+#define PRECISION 0.01 // Sets the precision
+
 
 typedef struct MatrixLocation {
+    // Defines indices for a location in a matrix
     long i;
     long j;
 } MatrixLocation;
 
 typedef struct ReadBatch {
+    // Defines data required to read batch from file in parallel
     size_t batchLength;
     MatrixLocation* matrixLocations;
     double** mat;
@@ -20,6 +23,7 @@ typedef struct ReadBatch {
 } ReadBatch;
 
 typedef struct WriteBatch {
+    // Defines data required to calculate and write data to mat in memory in parallel
     size_t batchLength;
     MatrixLocation* matrixLocations;
     double** mat;
@@ -28,6 +32,7 @@ typedef struct WriteBatch {
 } WriteBatch;
 
 double** initDoubleMatrix(size_t size) {
+    // Allocates memory for a double matrix of size*size elements
     double** mat = (double**) malloc(size * sizeof(double*));
     double* matBuf = (double*) malloc(size * size * sizeof(double));
     for (size_t i = 0; i < size; i++) {
@@ -37,11 +42,13 @@ double** initDoubleMatrix(size_t size) {
 }
 
 void freeDoubleMatrix(double** mat) {
+    // Frees memory allocated by initDoubleMatrix
     free(mat[0]);
     free(mat);
 }
 
 void logSquareDoubleMatrix(double** mat, size_t size) {
+    // Logs mat
     for (size_t i = 0; i < size; i++) {
         for (size_t j = 0; j < size; j++) {
             printf("%.2lf ", mat[i][j]);
@@ -51,34 +58,17 @@ void logSquareDoubleMatrix(double** mat, size_t size) {
     printf("\n");
 }
 
-pthread_mutex_t** initMutexMatrix(size_t size) {
-    pthread_mutex_t** mtxMat = (pthread_mutex_t**) malloc(size * sizeof(pthread_mutex_t*));
-    pthread_mutex_t* mtxMatBuf = (pthread_mutex_t*) malloc(size * size * sizeof(pthread_mutex_t));
-    for (size_t i = 0; i < size; i++) {
-        mtxMat[i] = (size * i) + mtxMatBuf;
-    }
-    for (size_t i = 0; i < size; i++) {
-        for (size_t j = 0; j < size; j++) {
-            pthread_mutex_init(&mtxMat[i][j], NULL);
-        }
-    }
-    return mtxMat;
-}
-
-void freeMutexMatrix(pthread_mutex_t** mtxMat) {
-    free(mtxMat[0]);
-    free(mtxMat);
-}
-
-double doubleMean(double mat[], int n) {
-    double matSum = 0.0;
+double doubleMean(double values[], int n) {
+    // Calculates mean of the n elements of values
+    double valuesSum = 0.0;
     for (int i = 0; i < n; i++) {
-        matSum += mat[i];
+        valuesSum += values[i];
     }
-    return matSum / n;
+    return valuesSum / n;
 }
 
 void timespecDifference(struct timespec start, struct timespec stop, struct timespec* delta) {
+    // Calculates the difference between two struct timespecs
     delta->tv_nsec = stop.tv_nsec - start.tv_nsec;
     delta->tv_sec  = stop.tv_sec - start.tv_sec;
     if (delta->tv_sec > 0 && delta->tv_nsec < 0) {
@@ -92,9 +82,11 @@ void timespecDifference(struct timespec start, struct timespec stop, struct time
 }
 
 void logDuration(size_t size, double duration, size_t threads) {
+    // Logs a double duration with some additional semantic info
     printf("%ldx%ld matrix converged at precision %lf in %lfs using %ld threads\n", size, size, PRECISION, duration, threads);
 }
 
 double doubleTime(struct timespec delta) {
+    // Converts timespec time to double time
     return (double) delta.tv_sec + ((double) delta.tv_nsec / BILLION);
 }
