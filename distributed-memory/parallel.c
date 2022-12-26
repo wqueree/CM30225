284ec594ea30a505fd5def54a46369d2a8b50b1f
Will Queree <wqueree@gmail.com>
2022-12-26 17:28:26 +0000

    Braindump

--- distributed-memory/parallel.c
+++ distributed-memory/parallel.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 #include <mpi.h>
 #include "utils.h"
 
@@ -23,16 +25,40 @@ int main(int argc, char** argv) {
             fscanf(dataFile, "%lf", &mat[i][j]);
         }
     }
-
-    MPI_init()
-
     fclose(dataFile);
+
+    // MPI Setup
+
+    char name[MPI_MAX_PROCESSOR_NAME];
+
+    int mpi_init_rc = MPI_Init(&argc, &argv);
+    if (mpi_init_rc != MPI_SUCCESS) {
+        printf ("Error starting MPI program\n");
+        MPI_Abort(MPI_COMM_WORLD, mpi_init_rc);
+    }
+
+    int mpi_rank, mpi_size, nproc, namelen;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size); 
+
+    printf("%d\n", mpi_rank);
+    if (mpi_rank == 0) {
+        printf("main reports %d procs\n", nproc);
+    }
+    namelen = MPI_MAX_PROCESSOR_NAME;
+    MPI_Get_processor_name(name, &namelen);
+    printf("hello world %d from %s\n", mpi_rank, name);
+
+    MPI_Finalize();
 
     // Timing
     struct timespec start, stop, delta;
 
     clock_gettime(CLOCK_REALTIME, &start);
     // relaxation(mat, size, n_threads, LOGGING);
+    for (int i = 0; i < 100000; i++) {
+        3+4;
+    }
     clock_gettime(CLOCK_REALTIME, &stop);
 
     timespecDifference(start, stop, &delta);
