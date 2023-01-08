d5a96d6bb254df0ba6e54b1ec646f6d74e423b3b
Will Queree <wqueree@gmail.com>
2023-01-08 15:40:35 +0000

    Formatting

--- distributed-memory/test-correctness/test-correctness.py
+++ distributed-memory/test-correctness/test-correctness.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 PRECISION = 0.01
 
+
 def write_square(size: int) -> Path:
     filename = f"{size}.txt"
     with open(file=filename, mode="w") as f:
@@ -18,33 +19,56 @@ def write_square(size: int) -> Path:
             f.write(f"{np.round(np.random.uniform(), 2)}\n")
     return Path(filename)
 
-def precision_equals(parallel_result: Iterable[float], serial_result: Iterable[float], precision: float) -> bool:
+
+def precision_equals(
+    parallel_result: Iterable[float], serial_result: Iterable[float], precision: float
+) -> bool:
     for (parallel_element, serial_element) in zip(parallel_result, serial_result):
         if abs(parallel_element - serial_element) > precision:
             return False
     return True
 
-def relaxation_compile() -> None:
-    subprocess.run(["gcc", "-fdiagnostics-color=always", "-g", "../serial.c", "-o", "../serial"])
-    subprocess.run(["mpicc", "-fdiagnostics-color=always", "-g", "../parallel.c", "-o", "../parallel"])
 
-def compute__results(test_data_filename: Path, serial_result_path: str, parallel_result_path: str) -> None:
-    with open(serial_result_path, "w") as serial_result_file:
-        subprocess.call(["../serial", str(test_data_filename)], stdout=serial_result_file)
-    with open(parallel_result_path, "w") as parallel_result_file:
-        subprocess.call(["mpirun", "-np", "4", "../parallel", str(test_data_filename)], stdout=parallel_result_file)
+def relaxation_compile() -> None:
+    subprocess.run(
+        ["gcc", "-fdiagnostics-color=always", "-g", "../serial.c", "-o", "../serial"]
+    )
+    subprocess.run(
+        [
+            "mpicc",
+            "-fdiagnostics-color=always",
+            "-g",
+            "../parallel.c",
+            "-o",
+            "../parallel",
+        ]
+    )
+
 
 def relaxation_test(test_data_filename: Path, precision: float) -> bool:
     serial_result_path = f"serial-{test_data_filename.stem}.out"
     parallel_result_path = f"parallel-{test_data_filename.stem}.out"
+
+    # Compute Results
     with open(serial_result_path, "w") as serial_result_file:
-        subprocess.call(["../serial", str(test_data_filename)], stdout=serial_result_file)
+        subprocess.call(
+            ["../serial", str(test_data_filename)], stdout=serial_result_file
+        )
     with open(parallel_result_path, "w") as parallel_result_file:
-        subprocess.call(["mpirun", "-np", "4", "../parallel", str(test_data_filename)], stdout=parallel_result_file)
+        subprocess.call(
+            ["mpirun", "-np", "4", "../parallel", str(test_data_filename)],
+            stdout=parallel_result_file,
+        )
+
+    # Check Results
     with open(serial_result_path, "r") as serial_result_file:
-        serial_result = map(float, serial_result_file.read().replace("\n", "").strip().split(" "))
+        serial_result = map(
+            float, serial_result_file.read().replace("\n", "").strip().split(" ")
+        )
     with open(parallel_result_path, "r") as parallel_result_file:
-        parallel_result = map(float, parallel_result_file.read().replace("\n", "").strip().split(" "))
+        parallel_result = map(
+            float, parallel_result_file.read().replace("\n", "").strip().split(" ")
+        )
 
     equal = precision_equals(parallel_result, serial_result, precision)
     os.remove(serial_result_path)
@@ -52,12 +76,15 @@ def relaxation_test(test_data_filename: Path, precision: float) -> bool:
     os.remove(test_data_filename)
     return equal
 
+
 def test_correctness(runs: int, precision: float) -> None:
     relaxation_compile()
     for i in range(runs):
         n = np.random.randint(20, 100)
         test_data_filename = write_square(n)
-        # print(f"Run {i + 1} ({n=}): {'PASS' if relaxation_test(test_data_filename, precision) else 'FAIL'}")
-        print(f"{i + 1} {n} {'Pass' if relaxation_test(test_data_filename, precision) else 'Fail'}")
+        print(
+            f"Test {i + 1} ({n=}): {'PASS' if relaxation_test(test_data_filename, precision) else 'FAIL'}"
+        )
+
 
 test_correctness(runs=40, precision=PRECISION)
